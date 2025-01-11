dd4e5dc6d2900139fb37fa2f8c617d5aeab08699
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-01-11 18:08:03 +0900

    Title: 개발자의 능력 3, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,24 @@
+import sys
+
+INT_MAX=sys.maxsize
+
+ans=INT_MAX
+
+abilities = list(map(int, input().split()))
+length=len(abilities)
+
+
+
+
+def differ(i,j,k):
+    team1=abilities[i]+abilities[j]+abilities[k]
+    team2=sum(abilities)-team1
+    return abs(team1-team2)
+
+for i in range(length):
+    for j in range(i,length):
+        for k in range(j,length):
+            ans=min(ans,differ(i,j,k))
+
+print(ans)
+            
