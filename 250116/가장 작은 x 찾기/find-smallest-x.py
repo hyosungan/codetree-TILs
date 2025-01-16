08e34a5d7c668859a3f81f76dfd03c0a1063adaa
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-01-16 16:07:37 +0900

    Title: 가장 작은 x 찾기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250116/가장 작은 x 찾기/find-smallest-x.py
+++ 250116/가장 작은 x 찾기/find-smallest-x.py
@@ -1,17 +1,18 @@
 n = int(input())
 ranges = [tuple(map(int, input().split())) for _ in range(n)]
 
-x=[i for i in range((ranges[0][1]))]
+x = 1  # 가능한 x의 최소값부터 시작
 
-for i,(a,b) in enumerate(ranges[:]):
-    for j in x:
-        if j*(2**(i+1))>=a and j*(2**(i+1))<=b:
-            pass
-        else:
-            x.remove(j)
-
-
-print(x[0])
-
-
-
+while True:
+    valid = True
+    current_value = x
+    for a, b in ranges:
+        if not (a <= current_value <= b):
+            valid = False
+            break
+        current_value *= 2  # 다음 단계로 값 업데이트
+    
+    if valid:
+        print(x)
+        break
+    x += 1
