5c48a21d605d5f8e2cb0e1f5fce129883f5de3e9
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-01-27 20:10:50 +0900

    Title: 연속된 숫자 만들기 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250127/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -4,8 +4,8 @@ pos.sort()
 
 if pos[2]-pos[0]==2:
     print(0)
-elif pos[2]-pos[0]==3:
-    print(1)
+# elif pos[2]-pos[0]==3:
+#     print(1)
 elif (pos[1]-pos[0])==2 or (pos[2]-pos[1])==2:
     print(1)
 else:
