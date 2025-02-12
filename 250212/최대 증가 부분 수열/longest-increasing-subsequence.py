9ced4cd40cc1da0b5c43e9ef22df71a8305ab328
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-12 09:37:07 +0900

    Title: 최대 증가 부분 수열, Time: 62ms, Memory: 17MB, Status: Passed - CodeTree

--- 250212/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 250212/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -11,6 +11,9 @@ for i in range(1,n):
     for j in range(0,i):
         if m[i]>m[j]:
             dp[i]=max(dp[i],dp[j]+1)
+    else:
+        if dp[i]==INT_MIN:
+            dp[i]=1
 
 ans=0
 for i in range(n):
