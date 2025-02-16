60c46ed1f80b46f8433965d27a229dcbde92e9de
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-16 11:15:05 +0900

    Title: 동전 거슬러주기, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250216/동전 거슬러주기/coin-change.py
+++ 250216/동전 거슬러주기/coin-change.py
@@ -1,22 +1,25 @@
 import sys
-sys.setrecursionlimit(20000)
 INT_MAX=sys.maxsize
 
 N, M = map(int, input().split())
 coin = list(map(int, input().split()))
-ans=INT_MAX
-def recur(value,count):
-    global ans
-    if value>=M:
-        if value==M:
-            ans=min(ans,count)
-        return
 
-    for co in coin:
-        recur(value+co,count+1)
+dp=list(INT_MAX for _ in range(M+1))
 
-recur(0,0)
-print(ans)
+dp[0]=0
+
+for i in range(1,M+1):
+    for j in range(N):
+        if i<coin[j]:
+            continue
+        if dp[i-coin[j]]==INT_MAX:
+            continue
+        dp[i]=min(dp[i-coin[j]]+1,dp[i])
+
+if dp[M]==INT_MAX:
+    print(-1)
+else:
+    print(dp[M])
 
 
 
