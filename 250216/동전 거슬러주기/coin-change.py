1d6fc3872563160a23367ced5b8528ff43d79c47
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-16 10:49:48 +0900

    Title: 동전 거슬러주기, Time: 119ms, Memory: 22MB, Status: Runtime Error - CodeTree

---
+++ 250216/동전 거슬러주기/coin-change.py
@@ -0,0 +1,22 @@
+import sys
+
+INT_MAX=sys.maxsize
+
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+ans=INT_MAX
+def recur(value,count):
+    global ans
+    if value>=M:
+        if value==M:
+            ans=min(ans,count)
+        return
+
+    for co in coin:
+        recur(value+co,count+1)
+
+recur(0,0)
+print(ans)
+
+
+
