9e95e514158277dd8919dabcfec6a1f326d33e51
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-19 11:48:07 +0900

    Title: 고대 보물 지도의 비밀, Time: 61ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 250219/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -0,0 +1,26 @@
+import sys
+INT_MIN=-sys.maxsize
+
+n, k = map(int, input().split())
+a = [0]+list(map(int, input().split()))
+
+dp=[0 for _ in range(n+1)]
+
+dp[0]=0
+
+count=0
+for i in range(1,n+1):
+    if a[i]<0:
+        count+=1
+    if count>k:
+        for j in range(i-1,0,-1):
+            if a[j]<0:
+                for k in range(j+1,i+1):
+                    dp[i]+=a[k]
+                break
+        count=1
+        continue
+    dp[i]=max(a[i],dp[i-1]+a[i])
+
+print(max(dp))
+
