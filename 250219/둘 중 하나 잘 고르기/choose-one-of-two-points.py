89c68db16abcf8eb7854f2aee0cfeb69143b763a
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-19 17:53:07 +0900

    Title: 둘 중 하나 잘 고르기, Time: 65ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250219/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -0,0 +1,29 @@
+n = int(input())
+red = [0]
+blue = [0]
+
+for _ in range(2 * n):
+    r, b = map(int, input().split())
+    red.append(r)
+    blue.append(b)
+
+dp=[[0 for _ in range(n+1)] for _ in range(2*n+1)]
+
+for i in range(1,2*n+1):
+    dp[i][0]=dp[i-1][0]+blue[i]
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j:
+            dp[i][j]=dp[i-1][j-1]+red[i]
+            break
+
+for i in range(1,2*n+1):
+    for j in range(1,n+1):
+        if i<j:
+            continue
+        dp[i][j]=max(dp[i-1][j-1]+red[i],dp[i-1][j]+blue[i])
+
+print(dp[2*n][n])
+
+
