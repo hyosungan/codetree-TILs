982590ea079b441b78d7caf07f23f1227ee474f1
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-19 17:57:15 +0900

    Title: 둘 중 하나 잘 고르기, Time: 68ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250219/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 250219/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -12,11 +12,6 @@ dp=[[0 for _ in range(n+1)] for _ in range(2*n+1)]
 for i in range(1,2*n+1):
     dp[i][0]=dp[i-1][0]+blue[i]
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j:
-            dp[i][j]=dp[i-1][j-1]+red[i]
-            break
 
 for i in range(1,2*n+1):
     for j in range(1,n+1):
