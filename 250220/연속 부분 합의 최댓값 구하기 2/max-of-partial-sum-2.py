f3caca11c48581a1c98847b6c9e60a9f064eb9dc
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-20 12:41:27 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

--- 250220/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 250220/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -4,11 +4,11 @@ INT_MIN=-sys.maxsize
 n = int(input())
 a = list(map(int, input().split()))
 count=0
-ans=0
-dp=[INT_MIN for _ in range(n+1)]
-dp[0]=0
+ans=INT_MIN
 
-for i in range(1,n+1):
-    dp[i]=max(dp[i-1]+a[i-1],a[i-1])
-
-print(max(dp[1:]))
+for i in range(n):
+    count+=a[i]
+    ans=max(ans,count)
+    if count<0:
+        count=0
+print(ans)
