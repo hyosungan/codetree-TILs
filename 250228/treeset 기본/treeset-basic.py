da08e067276be26de9d823032cf2b49bfa7a4c2e
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-02-28 17:09:37 +0900

    Title: treeset 기본, Time: 231ms, Memory: 24MB, Status: Passed - CodeTree

--- 250228/treeset 기본/treeset-basic.py
+++ 250228/treeset 기본/treeset-basic.py
@@ -7,16 +7,16 @@ for _ in range(n):
     command=input().split()
     kind=command[0]
     if kind=='add':
-        s.add(command[1])
+        s.add(int(command[1]))
     elif kind=='remove':
-        s.remove(command[1])
+        s.remove(int(command[1]))
     elif kind=='find':
-        print('true' if command[1] in s else 'false')
+        print('true' if int(command[1]) in s else 'false')
     elif kind=='lower_bound':
-        print(s[s.bisect_left(command[1])] if s.bisect_left(command[1])<len(s) else None)
+        print(s[s.bisect_left(int(command[1]))] if s.bisect_left(int(command[1]))<len(s) else None)
 
     elif kind=='upper_bound':
-        print(s[s.bisect_right(command[1])] if s.bisect_right(command[1])<len(s) else None)
+        print(s[s.bisect_right(int(command[1]))] if s.bisect_right(int(command[1]))<len(s) else None)
     elif kind=='largest':
         print(s[-1] if s else None)
     elif kind=='smallest':
