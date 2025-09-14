80bfe797c8de7ae501e4c0642ca464b4f9271bf1
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-09-14 22:35:35 +0900

    Title: 최소 스패닝 트리, Time: 661ms, Memory: 36MB, Status: Passed - Codetree

--- 250914/최소 스패닝 트리/minimum-spanning-tree.java
+++ 250914/최소 스패닝 트리/minimum-spanning-tree.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class main {
+public class Main {
     static int[] uf;
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
