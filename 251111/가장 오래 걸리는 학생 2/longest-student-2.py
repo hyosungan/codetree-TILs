36205254d12e2c9295a8333e7981b12c635faf6a
hyosungan <104579283+hyosungan@users.noreply.github.com>
2025-11-11 09:58:12 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 340ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 251111/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -0,0 +1,29 @@
+import sys,heapq
+
+N, M = map(int, input().split())
+pq=[]
+dist=[int(1e9) for _ in range(N+1)]
+dist[N]=0
+heapq.heappush(pq,(0,N)) #거리와 노드 번호
+
+edges=[[] for _ in range(N+1)]
+
+for _ in range(M):
+    a,b,d=map(int,input().split())
+    edges[b].append((a,d))
+
+while pq:
+    curr_dist,curr_node=heapq.heappop(pq)
+    if dist[curr_node]!=curr_dist:
+        continue
+    for next_node,next_dist in edges[curr_node]:
+        if dist[next_node]>curr_dist+next_dist:
+            dist[next_node]=curr_dist+next_dist
+            heapq.heappush(pq,(curr_dist+next_dist,next_node))
+
+print(max(dist[1:]))
+
+
+
+
+
